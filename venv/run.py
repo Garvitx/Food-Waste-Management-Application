cccb8fbd82c32e531294cb342d335feb4d55aa20
Garvitx <sgarvitg@gmail.com>
2024-08-07 22:48:34 +0530

    Update run.py

--- venv/run.py
+++ venv/run.py
@@ -1,4 +1,4 @@
-# run.py
+import os
 from app import create_app, db
 from app.models import User, Product
 
@@ -9,4 +9,5 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Product': Product}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
